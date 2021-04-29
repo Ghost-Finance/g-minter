95b219003cb64448c0983527e0048f5c581587da
Luan Silveira Pontolio <luan.pontolio@gmail.com>
2021-04-29 08:21:34 -0300

    Chore: Add theme ghost g-minter

--- frontend/src/AppTheme.tsx
+++ frontend/src/AppTheme.tsx
@@ -1,57 +1,8 @@
 import React from 'react'
 import App from './App'
-import {
-  createMuiTheme,
-  CssBaseline,
-  makeStyles,
-  ThemeProvider
-} from '@material-ui/core'
+import { CssBaseline, makeStyles, MuiThemeProvider } from '@material-ui/core'
 import { SnackbarProvider } from 'notistack'
-import {
-  TOKEN_NEUTRAL_COLOR_LOW_DARK,
-  TOKEN_NEUTRAL_COLOR_LOW_PURE,
-  TOKEN_NEUTRAL_COLOR_LOW_LIGHT,
-  TOKEN_NEUTRAL_COLOR_HIGH_DARK,
-  TOKEN_NEUTRAL_COLOR_HIGH_LIGHT,
-  TOKEN_NEUTRAL_COLOR_HIGH_PURE,
-  TOKEN_FEEDBACK_COLOR_POSITIVE_PURE,
-  TOKEN_FEEDBACK_COLOR_POSITIVE_DARK,
-  TOKEN_FEEDBACK_COLOR_NEGATIVE_PURE,
-  TOKEN_FEEDBACK_COLOR_NEGATIVE_DARK,
-  TOKEN_BACKGROUND_COLOR_DEFAULT
-} from './tokens'
-
-const theme = createMuiTheme({
-  palette: {
-    primary: {
-      main: TOKEN_NEUTRAL_COLOR_LOW_DARK,
-      dark: TOKEN_NEUTRAL_COLOR_LOW_PURE,
-      light: TOKEN_NEUTRAL_COLOR_LOW_LIGHT
-    },
-    secondary: {
-      main: TOKEN_NEUTRAL_COLOR_HIGH_PURE,
-      dark: TOKEN_NEUTRAL_COLOR_HIGH_DARK,
-      light: TOKEN_NEUTRAL_COLOR_HIGH_LIGHT
-    },
-    error: {
-      main: TOKEN_FEEDBACK_COLOR_NEGATIVE_PURE,
-      dark: TOKEN_FEEDBACK_COLOR_NEGATIVE_DARK
-    },
-    success: {
-      main: TOKEN_FEEDBACK_COLOR_POSITIVE_PURE,
-      dark: TOKEN_FEEDBACK_COLOR_POSITIVE_DARK
-    },
-    background: {
-      default: TOKEN_BACKGROUND_COLOR_DEFAULT
-    }
-  },
-  typography: {
-    fontFamily: 'Inter',
-    caption: {
-      color: '#fff'
-    }
-  }
-})
+import theme from './theme'
 
 const useStyles = makeStyles({
   root: {
@@ -66,14 +17,14 @@ const AppTheme = () => {
   const classes = useStyles()
 
   return (
-    <ThemeProvider theme={theme}>
+    <MuiThemeProvider theme={theme}>
       <CssBaseline />
       <SnackbarProvider maxSnack={1}>
         <div className={classes.root}>
           <App />
         </div>
       </SnackbarProvider>
-    </ThemeProvider>
+    </MuiThemeProvider>
   )
 }
 
