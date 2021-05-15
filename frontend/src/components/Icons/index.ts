a830a3986794eabfe14a93fe68be54215a596493
Luan Silveira Pontolio <luan.pontolio@gmail.com>
2021-05-14 22:07:21 -0300

    Chore: Add new icons

---
+++ frontend/src/components/Icons/index.ts
@@ -0,0 +1,7 @@
+import GhostIcon from './GhostIcon'
+import DaiIcon from './DaiIcon'
+import EtherIcon from './EtherIcon'
+import SynthsIcon from './SynthsIcon'
+import SpaceXIcon from './SpaceXIcon'
+
+export { GhostIcon, DaiIcon, EtherIcon, SynthsIcon, SpaceXIcon }
