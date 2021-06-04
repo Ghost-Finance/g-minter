de9803a501dc805ce9c29b3be00a2b466ab63f03
Luan Silveira Pontolio <luan.pontolio@gmail.com>
2021-06-03 23:58:57 -0300

    Chore: Add icon

--- frontend/src/components/Icons/index.ts
+++ frontend/src/components/Icons/index.ts
@@ -1,3 +1,4 @@
+import LogoIcon from './LogoIcon'
 import GhostIcon from './GhostIcon'
 import DaiIcon from './DaiIcon'
 import EtherIcon from './EtherIcon'
@@ -8,4 +9,4 @@ import BurnCardIcon from './BurnCardIcon'
 import RewardCardIcon from './RewardCardIcon'
 import SynthCardIcon from './SynthCardIcon'
 
-export { GhostIcon, DaiIcon, EtherIcon, SynthsIcon, SpaceXIcon, MintCardIcon, BurnCardIcon, RewardCardIcon, SynthCardIcon }
+export { LogoIcon, GhostIcon, DaiIcon, EtherIcon, SynthsIcon, SpaceXIcon, MintCardIcon, BurnCardIcon, RewardCardIcon, SynthCardIcon }
