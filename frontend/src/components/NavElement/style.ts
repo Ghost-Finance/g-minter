cdd94bac39519b3d7897ed9672d694b3377342da
luanpontolio <luan.pontolio@gmail.com>
2021-07-10 12:08:03 -0300

    Change transition page animation (#37)
    * Improve: Change transition page animation for ease-in and ease-in-out
    
    * Chore: Add GhostRatioSimulation component
    
    * Improve: Fix error changing text to div
    
    * Improve: some adjust by eslint
    
    * Chore: Add page mintr
    
    * Chore: Add NavElement style with banckgound and without background
    
    * Improve: Add rootPage verifiction when page changes
    
    * Improve: Add IconButton to close page

--- frontend/src/components/NavElement/style.ts
+++ frontend/src/components/NavElement/style.ts
@@ -1,4 +1,4 @@
-import { makeStyles, Theme } from '@material-ui/core'
+import { makeStyles, Theme } from '@material-ui/core';
 
 const useStyle = makeStyles((theme: Theme) => ({
   root: {
@@ -9,9 +9,19 @@ const useStyle = makeStyles((theme: Theme) => ({
   },
   drawerPaper: {
     width: 400,
-    zIndex: 1000,
+    zIndex: 100,
     left: '80px',
     backgroundColor: theme.palette.primary.dark,
+    [theme.breakpoints.down('md')]: {
+      width: 300,
+    },
+  },
+  drawerPaperWithoutBackground: {
+    width: 400,
+    zIndex: 100,
+    left: 0,
+    backgroundColor: 'transparent',
+    border: 0,
     [theme.breakpoints.down('md')]: {
       width: 300,
     },
@@ -23,6 +33,6 @@ const useStyle = makeStyles((theme: Theme) => ({
     paddingTop: '70px',
     padding: theme.spacing(1),
   },
-}))
+}));
 
-export default useStyle
+export default useStyle;
--- frontend/src/pages/BurnPage/index.tsx
+++ frontend/src/pages/BurnPage/index.tsx
@@ -1,15 +1,25 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import IconButton from '@material-ui/core/IconButton'
-import CloseIcon from '@material-ui/icons/Close'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Grid } from '@material-ui/core';
+import IconButton from '@material-ui/core/IconButton';
+import CloseIcon from '@material-ui/icons/Close';
+import useStyle from './style';
 
-const BurnPage = () => (
-  <div className="page">
-    <Link to="/">
-      <IconButton><CloseIcon/></IconButton>
-    </Link>
-    Burn
-  </div>
-)
+const BurnPage = () => {
+  const classes = useStyle();
+  return (
+    <div className="modal">
+      <Grid container direction="row" className={classes.root}>
+        <div>
+          <Link to="/">
+            <IconButton color="secondary">
+              <CloseIcon style={{ color: '#fff' }} />
+            </IconButton>
+          </Link>
+        </div>
+      </Grid>
+    </div>
+  );
+};
 
-export default BurnPage
+export default BurnPage;
