2cb8b38a3b81e0cd316a09b49eeb9dc7457c604b
luanpontolio <luan.pontolio@gmail.com>
2021-08-09 23:50:38 -0300

    Update solidity version ^0.8.0 (#40)
    * Improve: Remove UMA protocol
    
    * remove unnecessary files
    
    * Add solidity version 0.8.0
    
    * Add version 0.8.0

--- contracts/GDai.sol
+++ contracts/GDai.sol
@@ -1,12 +1,12 @@
 //SPDX-License-Identifier: MIT
-pragma solidity 0.8.6;
+pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
 
 contract GDai is ERC20, Ownable, Pausable {
-    
+
     constructor() ERC20("gDai", "GDAI") {
       _mint(msg.sender, 0);
     }
