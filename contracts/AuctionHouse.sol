7aae6b763a5257d99a8ed393bd96fae0f3275fd5
Luan Silveira Pontolio <luan.pontolio@gmail.com>
2021-08-23 14:20:48 -0300

    Improve: Change params to start auctionHouse

--- contracts/AuctionHouse.sol
+++ contracts/AuctionHouse.sol
@@ -1,4 +1,4 @@
-//SPDX-License-Identifier: lalala
+//SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 import './Token.sol';
@@ -7,14 +7,15 @@ contract AuctionHouse {
   struct Auction {
     address user;
     address tokenAddress; //cdpAccountAddress;
+    address collateralTokenAddress;
     address keeperAddress;
-    uint256 auctionTarget;
     uint256 collateralAmount;
-    uint256 initialPrice;
+    uint initialPrice;
     uint256 priceReductionRatio;
     address collateralFeedPrice;
-    uint256 startTimestamp;
-    uint256 endTimestamp;
+    address synthFeedPrice;
+    uint startTimestamp;
+    uint endTimestamp;
   }
 
   uint constant WAD = 10**18;
@@ -23,72 +24,94 @@ contract AuctionHouse {
 
   Auction[] public auctions;
 
-  event Start(address indexed cdp, address indexed keeper, uint256 auctionTarget, uint start, uint end);
-  event Take(uint256 indexed id, address indexed keeper, address indexed to, uint amount, uint totalAmount, uint end);
+  event Start(address indexed cdp, address indexed keeper, uint amount, uint start, uint end);
+  event Take(uint indexed id, address indexed keeper, address indexed to, uint amount, uint totalAmount, uint end);
 
   function start (
-    address user_,
-    address tokenAddress_,
-    address keeperAddress_,
-    uint auctionTarget_,
-    uint256 collateralAmount_,
-    uint initialPrice_, // pode ser de oracle
-    uint256 priceReductionRatio_, // 0.999999999999999999999999999 1% aa. step=90s
-    address collateralFeedPrice_
+      address user_,
+      address tokenAddress_,
+      address collateralTokenAddress_,
+      address keeperAddress_,
+      uint256 collateralAmount_,
+      uint initialPrice_, // pode ser de oracle
+      uint256 priceReductionRatio_, // 0.999999999999999999999999999 1% aa. step=90s
+      address collateralFeedPrice_,
+      address synthFeedPrice_
   ) public {
-    uint256 initialDate = block.timestamp;
-    uint256 endDate = initialDate + 1 weeks;
+      uint256 initialDate = block.timestamp;
+      uint256 endDate = initialDate + 1 weeks;
 
-    auctions.push(
-      Auction(
-        user_,
-        tokenAddress_,
-        keeperAddress_,
-        auctionTarget_,
-        collateralAmount_,
-        initialPrice_,
-        priceReductionRatio_,
-        collateralFeedPrice_,
-        initialDate,
-        endDate
-      )
-    );
+      auctions.push(
+          Auction(
+              user_,
+              tokenAddress_,
+              collateralTokenAddress_,
+              keeperAddress_,
+              collateralAmount_,
+              initialPrice_,
+              priceReductionRatio_,
+              collateralFeedPrice_,
+              synthFeedPrice_,
+              initialDate,
+              endDate
+          )
+      );
 
-    // emitir evento
-    emit Start(tokenAddress_, keeperAddress_, auctionTarget_, initialDate, endDate);
+      // emitir evento
+      emit Start(tokenAddress_, keeperAddress_, collateralAmount_, initialDate, endDate);
 
-    // tras o tokenToSell
-    require(Token(tokenAddress_).transferFrom(msg.sender, address(this), initialPrice_), "token transfer fail");
-    // require(Token(tokenAddress_).transferFrom(tx.origin, address(this), initialPrice_), "token transfer fail");
+      // tras o tokenToSell
+      require(Token(collateralTokenAddress_).transferFrom(msg.sender, address(this), collateralAmount_), "token transfer fail");
+      // require(Token(tokenAddress_).transferFrom(tx.origin, address(this), initialPrice_), "token transfer fail");
   }
 
   // auctionId,
   // amt amount para a comprar
   // receiver address
   // address timeHouse que deve retornar o preço atual do GDai
+  // liquidateDelinquentAccount
   function take(uint auctionId, uint amount, address receiver) public {
     Auction storage auction = auctions[auctionId];
     uint slice;
-    require(auction.auctionTarget > 0);
+    require(amount > 0);
     require(block.timestamp > auction.startTimestamp && block.timestamp < auction.endTimestamp);
-    uint totalAmountAuction = price(auction.initialPrice, block.timestamp - auction.auctionTarget, auction.priceReductionRatio) * auction.auctionTarget;
-    if (amount > totalAmountAuction) {
-      slice = totalAmountAuction;
+    // uint totalAmountAuction = price(auction.initialPrice, block.timestamp - auction.startTimestamp, auction.priceReductionRatio) * auction.auctionTarget;
+    if (amount > auction.collateralAmount) {
+        slice = auction.collateralAmount;
     } else {
-      slice = amount;
+        slice = amount;
     }
 
-    require(Token(auction.tokenAddress).transferFrom(msg.sender, receiver, slice), "token transfer fail");
+    // uint amountToFixCollateral = calculateAmountToFixCollateral(auction.initialPrice, slice);
 
+    // require(Token(auction.tokenAddress).transferFrom(msg.sender, receiver, slice), "token transfer fail");
     // etapas:
     //  - slice deve calcular
     auction.collateralAmount -= slice;
     if (auction.collateralAmount == 0) {
-      auction.endTimestamp = block.timestamp;
+        auction.endTimestamp = block.timestamp;
     }
 
     // emitir evento
     emit Take(auctionId, msg.sender, receiver, slice, auction.collateralAmount, auction.endTimestamp);
+  }
+
+  /**
+    * r = target issuance ratio
+    * D = debt balance
+    * V = Collateral
+    * P = liquidation penalty
+    * Calculates amount of synths = (D - V * r) / (1 - (1 + P) * r)
+    */
+  function calculateAmountToFixCollateral(uint debtBalance, uint collateral) public view returns (uint) {
+    uint ratio = 900; //getIssuanceRatio();
+
+    // uint dividend = debtBalance.sub(collateral.multiplyDecimal(ratio));
+    uint dividend = debtBalance - (collateral * ratio / WAD);
+    // uint divisor = unit.sub(unit.add(getLiquidationPenalty()).multiplyDecimal(ratio));
+    uint divisor = WAD - (WAD + WAD / 10) * ratio;
+
+    return dividend * WAD / divisor;
   }
 
   // top: initial price
