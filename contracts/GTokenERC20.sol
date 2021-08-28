611a32a3dd86befd6b03b0ff370cba586eab5570
Luan Silveira Pontolio <luan.pontolio@gmail.com>
2021-08-28 10:23:59 -0300

    Improve: Remove unnecessary method

--- contracts/GTokenERC20.sol
+++ contracts/GTokenERC20.sol
@@ -6,11 +6,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
 
 contract GTokenERC20 is ERC20, Ownable, Pausable {
-  constructor(string memory name_, string memory symbol_) ERC20(name_, symbol_) {
-    _mint(msg.sender, 100000000000000000000);
-  }
-
-  function mintMinerReward(uint256 initialSupply_) public onlyOwner {
-    _mint(block.coinbase, initialSupply_);
+  constructor(string memory name_, string memory symbol_, uint256 initialSupply) ERC20(name_, symbol_) {
+    _mint(msg.sender, initialSupply);
   }
 }
