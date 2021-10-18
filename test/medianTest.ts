49eb62256990804f61667559afba50b7b3b3a7e6
Luan Silveira Pontolio <luan.pontolio@gmail.com>
2021-10-18 15:54:39 -0300

    Test: Add test to verify signature

--- test/medianTest.ts
+++ test/medianTest.ts
@@ -4,25 +4,57 @@ import { BigNumber } from 'ethers';
 import { parseEther } from 'ethers/lib/utils';
 
 let medianContractLabel: string = 'Median';
+let signerContractLabel: string = 'Signature';
 
 describe('#Median', async function() {
-  let Median, median;
+  let Median,
+    Signature,
+    median,
+    signer,
+    mnemonic,
+    mnemonicWallet,
+    owner,
+    accountOne,
+    accountTwo,
+    accountThree,
+    accounts,
+    others;
 
   beforeEach(async function() {
-    const [owner, ...accounts] = await ethers.getSigners();
-    const [accountOne, accountTwo, accountThree, ...others] = accounts;
+    [owner, ...accounts] = await ethers.getSigners();
+    [accountOne, accountTwo, accountThree, ...others] = accounts;
 
     Median = await ethers.getContractFactory(medianContractLabel);
+    Signature = await ethers.getContractFactory(signerContractLabel);
     median = await Median.deploy();
+    signer = await Signature.deploy();
+
+    mnemonic =
+      'radar blur cabbage chef fix engine embark joy scheme fiction master release';
+    mnemonicWallet = ethers.Wallet.fromMnemonic(mnemonic);
   });
 
-  it('recovery', async function() {
+  it.only('#recovery', async function() {
+    const accountAddress = await accountOne.address;
+    const hash = await ethers.utils.keccak256(accountAddress);
+    const signature = await accountOne.signMessage(ethers.utils.arrayify(hash));
+    const publicKey = ethers.utils.recoverPrivateKey(hash, signature);
+    console.log(publicKey);
+    const [r, s, v] = await signer.extractRSV(publicKey);
+    console.log(r);
+    console.log(s);
+    console.log(v);
+
     const timestamp = new Date().getTime();
-    let messageHash = ethers.utils.solidityKeccak256([
-      BigNumber.from(parseEther('120.0')),
-      timestamp,
-      'spacex',
-    ]);
-    console.log(messageHash);
+    const expectedSignatureHash = ethers.utils.solidityKeccak256(
+      ['string', 'string', 'string'],
+      [BigNumber.from(parseEther('120.0')), timestamp, 'spacex']
+    );
+
+    const signatureAccountOne = await median
+      .connect(accountOne)
+      .recover(BigNumber.from(parseEther('120.0')), timestamp, v, r, s);
+
+    console.log(signatureAccountOne);
   });
 });
