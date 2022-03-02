93d27c860e1b82e9bc44bd81452a11cbd3537194
cTyl3r <chineblane@gmail.com>
2022-03-02 13:36:54 -0300

    Improve: Rename file

--- frontend/src/config/synths.ts
+++ frontend/src/config/synths.ts
@@ -1,24 +1,25 @@
 // background image
 import SpaceXBackground from '../assets/images/space-x.jpeg';
 // icons
-import { SpaceXIcon } from '../components/Icons';
+import SpaceXIcon from '../assets/spaceX-logo.svg';
 
 require('dotenv').config();
 
-export interface SythData {
+export interface SynthData {
   key: string;
   background: string;
-  logo: JSX.Element;
+  logo: string;
   title: string;
   subtitle: string;
+  amount?: number;
 }
 
 export const stakesData = [
   {
-    key: process.env.GSPACEX_KEY,
+    key: process?.env?.GSPACEX_KEY || '0x4753504143455800000000000000000000000000000000000000000000000000',
     background: SpaceXBackground,
-    logo: <SpaceXIcon />,
+    logo: SpaceXIcon,
     title: 'SpaceX',
     subtitle: 'gSPX',
-  } as SythData,
-];
+  },
+] as Array<SynthData>;
